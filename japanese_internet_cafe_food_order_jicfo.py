ae29e3569128d8986b4626555e2cae1d74925687
Akasaka <59734602+akasak4@users.noreply.github.com>
2023-09-27 15:06:11 +0800

    Update japanese_internet_cafe_food_order_jicfo.py
    updating the program with simple if-elif-else statement. the value of variable will display food name instead of number.

--- japanese_internet_cafe_food_order_jicfo.py
+++ japanese_internet_cafe_food_order_jicfo.py
@@ -1,11 +1,27 @@
 input("Lists Menu (Press Enter)")
 
-foods = ['Takoyaki', 'Tonkotsu Ramen', 'Yakitori', 'Okonomiyaki', 'Curry over rice (カレーライス)' ]
+foods = ['1. Takoyaki (たこ焼き)', '2. Tonkotsu Ramen (豚骨/とんこつ)', '3. Yakitori (焼き鳥)', '4. Okonomiyaki (お好み焼き)', '5. Curry over rice (カレーライス)' ]
 
-for index, food in enumerate(foods, start=1):
-    print(f"{index}. {food}")
+for food in foods:
+    print(food)
 
-order = input("Enter the number of food you want to order: ")
-foods.append(order)
+x = int(input("Enter the number of food you want to order: "))
 
-print(f"Thank you for ordering {order}, Have a nice day! Itadakimasu!")
+if x == 1:
+    y = foods[0]
+    print(f"\nThank you for order {y}")
+elif x == 2:
+    y = foods[1]
+    print(f"\nThank you for order {y}")
+elif x == 3:
+    y = foods[2]
+    print(f"\nThank you for order {y}")
+elif x == 4:
+    y = foods[3]
+    print(f"\nThank you for order {y}")
+elif x == 5:
+    y = foods[4]
+    print(f"\nThank you for order {y}")
+else :
+    print("Sorry, but your order is not on the List!")
+
